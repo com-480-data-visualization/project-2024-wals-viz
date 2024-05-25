cabae086f1d7c6519a2360e3c42e5c62aa153da6
samakovl <dimitrios.samakovlis@epfl.ch>
2024-05-25 16:51:09 +0200

    Added languages to the hierarchy + zoomable Sunburst + fine-tuned the genealogy section appearance

--- python/csv_to_hierarchical_json.py
+++ python/csv_to_hierarchical_json.py
@@ -9,7 +9,7 @@ def csv_to_hierarchical_json(csv_file, json_file):
     
     # Step 1 - Read the csv and keep only the columns of interest (i.e. genus, family, macroarea)
     df = pandas.read_csv(csv_file)
-    df = df[['genus', 'family', 'macroarea']]
+    df = df[['genus', 'family', 'macroarea', 'Name']]
 
     # Step 2 - Remove blank rows
     df = df.dropna()
@@ -38,18 +38,24 @@ def csv_to_hierarchical_json(csv_file, json_file):
             genuses_for_family = df[(df['macroarea'] == macroarea) & (df['family'] == family)]['genus'].unique()
             # print('\nGenuses for', family, ':', genuses_for_family)
 
-            # Count how many times each genus appears (leafs of the hierarchy)
-            leafs = []
+            # For each genus find the names of the languages
+            genuses = []
             for genus in genuses_for_family:
-                count = df[(df['macroarea'] == macroarea) & (df['family'] == family) & (df['genus'] == genus)].shape[0]
-                # print(genus, 'appears', count, 'times')
-                leafs.append({"name": genus, "value": count})   # leaf nodes of the hierarchy
+                
+                languages = []
+                # Construct the leaf nodes (language with count 1)
+                for index, row in df.iterrows():
+                    if row['macroarea'] == macroarea and row['family'] == family and row['genus'] == genus:
+                        name = row['Name']
+                        languages.append({"name": name, "value": 1})  # leaf nodes of the hierarchy
+
+                genuses.append({"name": genus, "children": languages})   # grandchildren nodes of the hierarchy
 
             # append an entry for the current genus to the children array of the correct genus
-            children.append({"name": family, "children": leafs})    # parent nodes of the hierarchy
+            children.append({"name": family, "children": genuses})    # parent nodes of the hierarchy
 
         # Append the children array to the current macroarea
-        hierarchical_json.append({"name": macroarea, "children": children})   # granparent nodes of the hierarchy
+        hierarchical_json.append({"name": macroarea, "children": children})   # grandparent nodes of the hierarchy
 
         # print('\nHierarchical JSON:', hierarchical_json)
         # input("Press Enter to continue...")
